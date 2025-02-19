80e2712dd0a8032a40446668ae7b0036616f5388
vansh-tambi <23u02084@iiitbhopal.ac.in>
2025-02-19 11:35:23 +0530

    Updated OTP.js

--- models/OTP.js
+++ models/OTP.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+
 const OTPSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -15,5 +16,15 @@ const OTPSchema = new mongoose.Schema({
         expires: 5*60
     }
 })
+
+async function sendVerificationEmail(email , otp) {
+    try{
+        const mailResponse = await mailSend
+    }
+    catch(err){
+        console.log("Error occured while sending mail", err);
+        throw err;
+    }
+}
 
 module.exports = mongoose.model("Profile" , OTPSchema);
