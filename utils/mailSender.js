7da2140d8a45dfc55076c6f223d2db2e5f1c78f8
vansh-tambi <23u02084@iiitbhopal.ac.in>
2025-02-19 11:25:25 +0530

    Updated mailSender.js

---
+++ utils/mailSender.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const mailSender = async(email , title , body) => {
+    try{
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        })
+        let info = await transporter.sendMail({
+            from: "StudyNotion || Vansh Tambi",
+            to:`$email`,
+            subject:`$title`,
+            html: `$body`
+        })
+        console.log(info);
+    }
+    catch(err){
+        console.log("Error in sending OTP" , err);
+    }
+}
